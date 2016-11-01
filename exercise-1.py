de1887b59ddb2b33da6a5616512136a6763c24df
Babasanya <babscraig@gmail.com>
2016-11-01 21:37:53 +0100

    Exercise-1

---
+++ exercise-1.py
@@ -0,0 +1,15 @@
+# The following tasks have been completed:
+#
+# Go to https://atom.io/ with your browser, get the Atom text editor, and install it.
+# Put Atom (your editor) in your dock so you can reach it easily.
+# Find your Terminal program. Search for it. You will find it.
+# Put your Terminal in your dock as well.
+# Run your Terminal program. It won't look like much.
+# In your Terminal program, run python. You run things in Terminal by just typing the name and hitting RETURN.
+# Type quit(), Enter, and get out of python.
+# You should be back at a prompt similar to what you had before you typed python. If not, find out why.
+# Learn how to make a directory in the Terminal.
+# Learn how to change into a directory in the Terminal.
+# Use your editor to create a file in this directory. You will make the file, "Save" or "Save As...," and pick this directory.
+# Go back to Terminal using just the keyboard to switch windows.
+# Back in Terminal, list the directory with ls to see your newly created file.
