8a2e1b57f697210a0d45b00577d5171a6f951e96
Babasanya <babscraig@gmail.com>
2016-11-04 18:33:41 +0100

    Day 4.5
    Combine raw_input with argv to make a script that gets more input from
    a user. Don't over think it. Just use argv to get something, and
    raw_input to get something else from the user.

---
+++ py_form.py
@@ -0,0 +1,8 @@
+print "Tell me your name",
+info = raw_input()
+print "What is your favourite colour?",
+colour = raw_input()
+print "Where do you live?"
+address = raw_input()
+
+print "So now we are friends, I know your name, %r, I know your favourite colour is %r and I know you live at %r" % (info, colour, address)
